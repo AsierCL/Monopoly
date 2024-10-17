7ba949cfa12bc46cff6d3f5a112aa31f51717b71
AsierCL <asiercl.05@gmail.com>
2024-10-17 09:32:30 +0200

    Asier: Cambios en restricciones construir, fix metodo estaconstruido()

--- monopoly/Edificios.java
+++ monopoly/Edificios.java
@@ -8,92 +8,102 @@ public class Edificios {
     private int hoteles;
     private int piscinas;
     private int pistas;
+    private int num_max;
 
     public Edificios(Casilla casilla){
         this.casilla = casilla;
-        this.casas = 0;
-        this.hoteles = 0;
-        this.piscinas = 0;
-        this.piscinas = 0;
+        casas = 0;
+        hoteles = 0;
+        piscinas = 0;
+        piscinas = 0;
     }
 
     public int getCasas() {
         return casas;
     }
+
     public int getHoteles() {
         return hoteles;
     }
+    
     public int getPiscinas() {
         return piscinas;
     }
+    
     public int getPistas() {
         return pistas;
     }
+    
     public Casilla getCasilla() {
         return casilla;
+    }
+    
+    public int getNum_max() {
+        return num_max;
+    }
+    
+    public void setNum_max(int num_max) {
+        this.num_max = num_max;
     }
 
     public boolean EstaEdificado(){
         if(casas==0 && hoteles==0 && piscinas==0 && piscinas==0){
-            return true;
-        }else{
             return false;
+        }else{
+            return true;
         }
     }
 
     public boolean ConstruirCasa(){
-        if(this.casas < 4){
-            this.casas += 1;
-            return true;
-        }else{
+        if(casas == num_max && hoteles == num_max){
+            System.out.println("No puedes construir más casas en este solar");
+            return false;
+        }else if(casas == 4 && hoteles < num_max){
             System.out.println("Debes construir un hotel");
             return false;
+        }else{
+            casas += 1;
+            return true;
         }
     }
 
     public boolean ConstruirHotel(){
-        if(this.hoteles < 3){
-            if(this.casas == 4){
-                this.hoteles += 1;
-                this.casas = 0;
-                return true;
-            }else{
-                System.out.println("Debes tener 4 casas antes");
-                return false;
-            }
-        }else{
-            System.out.println("No puedes construir más hoteles");
+        if(hoteles == num_max){
+            System.out.println("No puedes construir más hoteles en este solar");
             return false;
+        }else if(hoteles < num_max && casas != 4){
+            System.out.println("Debes tener 4 casas antes");
+            return false;
+        }else{
+            hoteles += 1;
+            casas = 0;
+            return true;
         }
     }
 
     public boolean ConstruirPiscina(){
-        if(this.hoteles >= 1 && this.casas >= 2 || this.hoteles >= 2){
-            if(this.piscinas < 2){
-                this.piscinas += 1;
-                return true;
-            }else{
-                System.out.println("No puedes tener más piscinas");
-                return false;
-            }
-        }else{
+        if(piscinas == num_max){
+            System.out.println("No puedes tener más piscinas en este solar");
+            return false;
+        }else if(hoteles == 0 || hoteles == 1 && casas < 2){
             System.out.println("Debes tener, como mínimo, 1 hotel y 2 casas");
             return false;
+        }else{
+            piscinas += 1;
+            return true;
         }
     }
 
     public boolean ConstruirPista(){
-        if(this.hoteles >= 2){
-            if(this.pistas < 2){
-                this.pistas += 1;
-                return true;
-            }else{
-                System.out.println("No puedes tener más pistas");
-                return false;
-            }
-        }else{
+        if(pistas == num_max){
+            System.out.println("No puedes tener más pistas en este solar");
+            return false;
+        }else if(hoteles < 2){
             System.out.println("Debes tener, como mínimo, 2 hoteles");
             return false;
+        }else{
+            pistas += 1;
+            return true;
         }
     }
 }
--- monopoly/Menu.java
+++ monopoly/Menu.java
@@ -437,11 +437,14 @@ public class Menu {
     * Parámetro: cadena de caracteres con el nombre de la casilla.
      */
     private void comprar(String nombre) {
-
+        Jugador jugador_actual = jugadores.get(turno);
         Casilla casilla_compra = tablero.obtenerCasilla(nombre);
-        Jugador jugador_compra = jugadores.get(turno);
 
-        casilla_compra.comprarCasilla(jugador_compra, banca);
+        if(casilla_compra != null && jugador_actual != null){
+            casilla_compra.comprarCasilla(jugador_actual, banca);
+        }else{
+            System.out.println("Casilla no encontrada");
+        }
     }
 
     //Método que ejecuta todas las acciones relacionadas con el comando 'salir carcel'. 
